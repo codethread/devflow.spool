33e73ee9102117ecca2aa822078be4e7afd9fe61
codethread <adamhalldesigns@gmail.com>
2026-08-15 21:43:21 +0100

    chore: adopt unified Codethread config

--- .millstrand/init.clj
+++ .millstrand/init.clj
@@ -7,12 +7,6 @@
   {:ns 'millstrand.spools.batteries
    :spools ['millstrand.spools/batteries]
    :required? true})
-(runtime/module! runtime :codethread/config
-  {:ns 'ct.spools.codethread.config
-   :spools ['codethread/config 'millstrand.spools/batteries]
-   :after [:millstrand/spools-batteries]
-   :required? true})
-
 ;; Devflow is a module so its named workflow routes are published as one
 ;; owner-complete contribution. Keep workflow first: it declares the route kind.
 (runtime/module! runtime :millhouse/spools-workflow
@@ -106,28 +100,41 @@
            :millhouse/spools-workflow]
    :required? true})
 
-(runtime/module! runtime :codethread/agents
-  {:ns 'ct.spools.codethread.agents
-   :spools ['codethread/agents 'ct.spools/agent-run 'ct.spools/delegation]
-   :after [:millstrand/spools-agent-run
-           :millstrand/spools-delegation]
-   :required? true})
-
 (runtime/module! runtime :millstrand/spools-subagent
   {:ns 'ct.spools.executors.subagent
    :spools ['ct.spools/agent-run 'millhouse.spools/workflow]
    :after [:millstrand/spools-agent-run
            :millhouse/spools-workflow
-           :codethread/agents
+           :codethread/config
            :devflow/kanban-adapter]
    :required? true})
 
-(runtime/module! runtime :codethread/devflow-setup
-  {:ns 'ct.spools.codethread.devflow-setup
-   :spools ['codethread/devflow-setup]
-   :after [:devflow/kanban-adapter]
+(runtime/module! runtime :codethread/config-agents
+  {:ns 'ct.spools.codethread.agents
+   :spools ['codethread/config 'ct.spools/agent-run]
+   :after [:millstrand/spools-agent-run]
    :required? true})
-
+(runtime/module! runtime :codethread/config-help
+  {:ns 'ct.spools.codethread.help
+   :spools ['codethread/config 'millstrand.spools/batteries]
+   :after [:millstrand/spools-batteries]
+   :required? true})
+(runtime/module! runtime :codethread/config-devflow
+  {:ns 'ct.spools.codethread.devflow
+   :spools ['codethread/config]
+   :required? true})
+(runtime/module! runtime :codethread/config
+  {:ns 'ct.spools.codethread.config
+   :spools ['codethread/config 'millstrand.spools/batteries
+            'ct.spools/agent-run 'ct.spools/delegation]
+   :after [:codethread/config-agents
+           :codethread/config-help
+           :codethread/config-devflow
+           :millstrand/spools-batteries
+           :millstrand/spools-agent-run
+           :millstrand/spools-delegation
+           :devflow/kanban-adapter]
+   :required? true})
 (runtime/module! runtime :codethread/ralph
   {:ns 'ct.spools.codethread.ralph
    :spools ['codethread/ralph 'millhouse.spools/workflow]
